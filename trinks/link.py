3055e4c828c07920478cf88c1cf2ab0397923ae1
fanecovsf <gustavo_h07@hotmail.com>
2023-07-11 07:10:52 -0300

    Feature: Final version

--- trinks/link.py
+++ trinks/link.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common import keys
+from selenium.webdriver.support import expected_conditions as EC
 import time
 
 class Link:
--- trinks/main.py
+++ trinks/main.py
@@ -2,29 +2,29 @@ import time
 
 from link import Link
 
+def trinks_extract():
+    trinks = Link("https://www.trinks.com/Login", "Chrome", sleep=1, headless=False)
 
-link = Link("https://www.trinks.com/Login", "Chrome", sleep=1, headless=False)
+    trinks.openLink()
 
-link.openLink()
+    trinks.maximize()
 
-link.maximize()
+    trinks.sendKeys('//*[@id="fEmail"]', 'matheus.cavichione@hotmail.com')
 
-link.sendKeys('//*[@id="fEmail"]', 'matheus.cavichione@hotmail.com')
+    trinks.sendKeys('//*[@id="fSenha"]', '@mcdlemao0505')
 
-link.sendKeys('//*[@id="fSenha"]', '@mcdlemao0505')
+    trinks.clickElement('//*[@id="login"]/div/div/form/div[3]/div[2]/button')
 
-link.clickElement('//*[@id="login"]/div/div/form/div[3]/div[2]/button')
+    trinks.clickElement('//*[@id="globalsite"]/section/nav/ul/li/div[5]/div[1]/a')
 
-link.clickElement('//*[@id="globalsite"]/section/nav/ul/li/div[5]/div[1]/a')
+    trinks.clickElement('//*[@id="globalsite"]/section/nav/ul/li/div[5]/div[2]/ul[1]/li/a')
 
-link.clickElement('//*[@id="globalsite"]/section/nav/ul/li/div[5]/div[2]/ul[1]/li/a')
+    trinks.clickElement('//*[@id="globalsite"]/section/nav/ul/li/div[5]/div[2]/ul[1]/li/ul/li[3]/a/span[2]')
 
-link.clickElement('//*[@id="globalsite"]/section/nav/ul/li/div[5]/div[2]/ul[1]/li/ul/li[3]/a/span[2]')
+    trinks.clickElement('/html/body/div[3]/div[2]/div/div/div[4]/div[2]/div[2]/input')
 
-link.clickElement('/html/body/div[3]/div[2]/div/div/div[4]/div[2]/div[2]/input')
+    trinks.clickElement('//*[@id="ExportarRankingDeProfissionaisPopup"]/section/div/button[1]')
 
-link.clickElement('//*[@id="ExportarRankingDeProfissionaisPopup"]/section/div/button[1]')
+    trinks.clickElement('/html/body/div[3]/div[2]/div/div/div[4]/div[2]/div[1]/input')
 
-link.clickElement('/html/body/div[3]/div[2]/div/div/div[4]/div[2]/div[1]/input')
-
-time.sleep(3000)
+trinks_extract()
